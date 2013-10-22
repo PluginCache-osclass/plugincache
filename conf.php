3be08e0e86ccb05b8503e51c0387072617414697
dev101-osclass <businesss.mail@gmail.com>
2013-10-22 22:20:06 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,4 +1,23 @@
 <?php
+    /*
+     *      OSCLass – software for creating and publishing online classified
+     *                           advertising platforms
+     *
+     *                        Copyright (C) 2010 OSCLASS
+     *
+     *       This program is free software: you can redistribute it and/or
+     *     modify it under the terms of the GNU Affero General Public License
+     *     as published by the Free Software Foundation, either version 3 of
+     *            the License, or (at your option) any later version.
+     *
+     *     This program is distributed in the hope that it will be useful, but
+     *         WITHOUT ANY WARRANTY; without even the implied warranty of
+     *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *             GNU Affero General Public License for more details.
+     *
+     *      You should have received a copy of the GNU Affero General Public
+     * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
 	if(Params::getParam('plugin_action')=='done') {
 	osc_set_preference('search_time', Params::getParam('search_time'), 'plugincache', 'INTEGER');
 	osc_set_preference('item_time', Params::getParam('item_time'), 'plugincache', 'INTEGER');
