3a293073dc30be2a12e8dded6727155ed2f1d2a5
cartagena68 <francesco.borasio@gmail.com>
2013-10-24 12:30:41 -0500

    Update help.php

--- admin/help.php
+++ admin/help.php
@@ -32,7 +32,7 @@
                 <p>the time setting is based on hours, this means that to set one hour you enter 1, to set one day you enter 24, to set one week you enter 168 and so on. You can also set less than one hour, for example if you want 15 minutes you can enter 0.25, 30 minutes 0.5 etc.  </p>
                 <br/>
                 <h3>Flexibility on which pages enable the cache</h3>
-                <p>The plugin will make the cache of 4 pages (Items Pages - Search Pages - Main Pages - Static Pages), From the setting page of the plugin you can enable/disable the cache in each one of the pages. </p>
+                <p>The plugin will make the cache of 3 pages (Items Pages - Main Pages - Static Pages), From the setting page of the plugin you can enable/disable the cache in each one of the pages. </p>
                 <br/>
                 <h3>Clean the cache folders</h3>
                 <p>You can also delete the files in each one of the folders in the plugin setting. It will show a list with de deleted files if any.</p>
@@ -43,11 +43,10 @@
                 <h3>Setting the item cache</h3>
                 <p>From plugin setting you can choose how the subfolder are created: by year, by month or by day</p>
                 <br/>
-                <h3>Clear the cache in main and search folders when a new ad is posted</h3>
+                <h3>Clear the cache in main folder when a new ad is posted</h3>
                 <p>From plugin setting you have the option to enable or disable this function. Useful if your site has high traffic and many new ads posted daily</p> 
             </fieldset>
         </div>
         <div style="clear: both;"></div>										
     </div>
 </div>
-
