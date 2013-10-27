bfc64472823f874e9d2273652b7c21c85462e8c7
cartagena68 <francesco.borasio@gmail.com>
2013-10-26 22:04:55 -0500

    Update conf.php
    Some clean up of code

--- admin/conf.php
+++ admin/conf.php
@@ -18,41 +18,39 @@
      *      You should have received a copy of the GNU Affero General Public
      * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
      */
+
         if(Params::getParam('plugin_action')=='done') {
-      //  osc_set_preference('search_time', Params::getParam('search_time'), 'cacheplugin', 'INTEGER'); REMOVED UNTIL BETTER SOLUTION
         osc_set_preference('item_time', Params::getParam('item_time'), 'cacheplugin', 'INTEGER');
         osc_set_preference('main_time', Params::getParam('main_time'), 'cacheplugin', 'INTEGER');
         osc_set_preference('static_time', Params::getParam('static_time'), 'cacheplugin', 'INTEGER');
         osc_set_preference('main_cache', Params::getParam('main_cache'), 'cacheplugin', 'INTEGER');
         osc_set_preference('item_cache', Params::getParam('item_cache'), 'cacheplugin', 'INTEGER');
-       // osc_set_preference('search_cache', Params::getParam('search_cache'), 'cacheplugin', 'INTEGER'); REMOVED UNTIL BETTER SOLUTION
         osc_set_preference('static_cache', Params::getParam('static_cache'), 'cacheplugin', 'INTEGER');
 		osc_set_preference('posted_item_clean_cache', Params::getParam('posted_item_clean_cache'), 'cacheplugin', 'INTEGER');
         osc_set_preference('item_storage_folder', Params::getParam('item_storage_folder'), 'cacheplugin', 'STRING');
-        echo '<div style="text-align:center; font-size:22px; background-color:#00bb00;"><p>' . __('Congratulations. The plugin is now configured', 'cacheplugin') . '.<a href="#" title="Close Message" onclick="parentNode.remove()" style="float:right;font-weight:bold;padding-right:50px;color:#FFFFFF;">'. __('x', 'cacheplugin').'</a></p></div>';
+			echo '<div style="text-align:center; font-size:22px; background-color:#00bb00;"><p>' . __('Congratulations. The plugin is now configured', 'cacheplugin') . '.<a href="#" title="Close Message" onclick="parentNode.remove()" style="float:right;font-weight:bold;padding-right:50px;color:#FFFFFF;">'. __('x', 'cacheplugin').'</a></p></div>';
+		   
         osc_reset_preferences();
         } else if(Params::getParam('plugin_action')=='clear') {
         if(Params::getParam('item')==1) {
-                cacheplugin_clear_item();
+			cacheplugin_clear_item();
         }
         if(Params::getParam('static')==1) {
-                cacheplugin_clear_static();
-        }
-        /* if(Params::getParam('search')==1) {
-                cacheplugin_clear_search();
-        } */
+			cacheplugin_clear_static();
+        }  
         if(Params::getParam('main')==1) {
-                cacheplugin_clear_main();
+			 cacheplugin_clear_main();
         }
-        echo '<div style="text-align:center; font-size:22px; background-color:#00bb00;"><p>' . __('The selected cache has been deleted', 'cacheplugin') . '.<a href="#" title="Close Message" onclick="parentNode.remove()" style="float:right;font-weight:bold;padding-right:50px;color:#FFFFFF;">'. __('x', 'cacheplugin').'</a></p></div>' ;
+			echo '<div style="text-align:center; font-size:22px; background-color:#00bb00;"><p>' . __('The selected cache has been deleted', 'cacheplugin') . '.<a href="#" title="Close Message" onclick="parentNode.remove()" style="float:right;font-weight:bold;padding-right:50px;color:#FFFFFF;">'. __('x', 'cacheplugin').'</a></p></div>' ;
         }
 ?>
 
+
+
 <div>
 	<div id="settings_form" style="width:100%; height:30px; padding:5px; border:1px solid #CCC; border-radius:5px; background:#EEE; font-size:18px; font-weight:bold;">
-													<legend><?php _e('Cache Plugin Settings', 'cacheplugin'); ?></legend>
+									<legend><?php _e('Cache Plugin Settings', 'cacheplugin'); ?></legend>
 	</div>
-
 	<div style="padding:10px 10px 10px 0px;">
 		<div style="float: left; width: 100%;">
 			<table width="100%">
@@ -79,13 +77,6 @@
 									<input type="radio" name="item_cache" id="item_cache" value="inactive" <?php if ($selectItemCache=='inactive') { echo ' checked'; } ?> /><span>Inactive</span>
 											<br/>
 											<br/>
-									<?php /* <label for="search_cache" style="font-weight:700;"><?php _e('Cache For Search Page', 'cacheplugin'); ?></label>
-											<br/>
-									<?php $selectSearchCache = osc_get_preference('search_cache', 'cacheplugin') ; ?>
-									<input type="radio" name="search_cache" id="search_cache" value="active" <?php if ($selectSearchCache=='active') { echo ' checked'; } ?> /><span>Active</span>
-									<input type="radio" name="search_cache" id="search_cache" value="inactive" <?php if ($selectSearchCache=='inactive') { echo ' checked'; } ?> /><span>Inactive</span>
-											<br/>
-											<br/> */ ?>
 									<label for="static_cache" style="font-weight:700;"><?php _e('Cache For Static Page', 'cacheplugin'); ?></label>
 											<br/>
 									<?php $selectStaticCache = osc_get_preference('static_cache', 'cacheplugin') ; ?>
@@ -94,19 +85,16 @@
 											<br/>
 											<br/>
 									<hr style="height:1px; border:none; color:#DDD; background-color:#DDD; border-color:#DDD; margin:20px 0;">
-								</div>
-								
+								</div>							
 								<div>
 									<label for="posted_item_clean_cache" style="font-weight:700;"><?php _e('Clear main cache when a new ad is posted.<br/>(Is better to set "Disabled" for sites with many new ads added daily)', 'cacheplugin'); ?></label>
 										<br/>
 									<?php $selectposteditemcleancache = osc_get_preference('posted_item_clean_cache', 'cacheplugin') ; ?>
 									<input type="radio" name="posted_item_clean_cache" id="posted_item_clean_cache" value="active" <?php if ($selectposteditemcleancache=='active') { echo ' checked'; } ?> /><span>Enabled</span>
 									<input type="radio" name="posted_item_clean_cache" id="posted_item_clean_cache" value="inactive" <?php if ($selectposteditemcleancache=='inactive') { echo ' checked'; } ?> /><span>Disabled</span>
-										<br/>
-												
+										<br/>												
 									<hr style="height:1px; border:none; color:#DDD; background-color:#DDD; border-color:#DDD; margin:20px 0;">
 								</div>
-
 								<div>
 									<label for="item_storage_folder" style="font-weight:700;"><?php _e('Select how you want to split the item folder<br/>(Based on item publication date)', 'cacheplugin'); ?></label>
 										<br/>
@@ -117,19 +105,13 @@
 										<br/>
 									<hr style="height:1px; border:none; color:#DDD; background-color:#DDD; border-color:#DDD; margin:20px 0;">
 								</div>
-
 								<div>
-										<p style="font-weight:700;"><?php _e('Regeneration Intervals', 'cacheplugin'); ?></p>
-										<label for="main_time"><?php _e('Time before re-generation of cache<br/>files for main page (In Hours)', 'cacheplugin'); ?></label>
+									<p style="font-weight:700;"><?php _e('Regeneration Intervals', 'cacheplugin'); ?></p>
+									<label for="main_time"><?php _e('Time before re-generation of cache<br/>files for main page (In Hours)', 'cacheplugin'); ?></label>
 											<br/>
 										<input type="text" name="main_time" id="main_time" value="<?php echo osc_get_preference('main_time', 'cacheplugin'); ?>"/>
 											<br/>
 											<br/>
-										<?php /* <label for="search_time"><?php _e('Time before re-generation of cache<br/>files for search results (In Hours) ', 'cacheplugin'); ?></label>
-											<br/>
-										<input type="text" name="search_time" id="search_time" value="<?php echo osc_get_preference('search_time', 'cacheplugin'); ?>"/>
-											<br/>
-											<br/> */ ?>
 										<label for="item_time"><?php _e('Time before re-generation of cache files<br/>for item\'s page (In Hours)', 'cacheplugin'); ?></label>
 											<br/>
 										<input type="text" name="item_time" id="item_time" value="<?php echo osc_get_preference('item_time', 'cacheplugin'); ?>"/>
@@ -140,13 +122,11 @@
 										<input type="text" name="static_time" id="static_time" value="<?php echo osc_get_preference('static_time', 'cacheplugin'); ?>"/>
 										<hr style="height:1px; border:none; color:#DDD; background-color:#DDD; border-color:#DDD; margin:20px 0;">
 								</div>
-
 								<p><button style="font-size:16px; font-weight:700;" type="submit" ><?php _e('Update', 'cacheplugin');?></button></p>
 											<br/>
 							</form>
 						</fieldset>
 					</td>
-
 					<td align="center" style="float:left; margin-left:10px; border:3px solid #888888; border-radius:5px; padding:15px;">     
 						<table>
 								<p style=" font-size:16px; font-weight:700;"><?php _e('Delete Cached Files', 'cacheplugin'); ?></p>
@@ -178,17 +158,6 @@
 										<span><?php _e("Clear cache of main page"); ?></span><br/><br/>
 									</td>
 								</tr>
-								<?php /*<tr style="vertical-align: top;">
-									<td>
-										<span style="font-weight:700;"><?php _e("Search Pages"); ?></span>
-									</td>
-								</tr>
-								 <tr style="vertical-align: top;">
-									<td>
-										<input type="checkbox" name="search" value="1">
-										<span><?php _e("Clear cache of searh pages"); ?></span><br/><br/>
-									</td>
-								</tr> */ ?>
 								<tr style="vertical-align: top;">
 									<td>
 										<span style="font-weight:700;"><?php _e("Static Pages"); ?></span>
